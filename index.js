ae60c271a78f1b57c68dee83b14146aecbb2076e
chandra <chandra00bond@gmail.com>
2024-04-14 23:43:23 +0530

    backened finished

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const PORT = process.env.PORT || 5000;
+const app = express();
+const router = require("./router");
+const server = http.createServer(app);
+const cors = require("cors");
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+
+// const io = socketio(server);
+app.use(cors());
+
+const io = socketio(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(router);
+io.on("connection", (socket) => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+    // adduser function two things one is error and other is user
+
+    if (error) return callback(error);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name}, has joined` });
+
+    socket.join(user.room);
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} has left`,
+      });
+    }
+  });
+});
+
+// this code establishes a WebSocket server using socket.io, logs messages when users connect and disconnect, and listens for events emitted by clients. It forms the basis for real-time bidirectional communication between clients and the server.
+
+server.listen(PORT, () => {
+  console.log(`Server is started on port ${PORT}`);
+});
